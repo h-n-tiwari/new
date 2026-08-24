c51ddafd02220394b6bd8b1b0468b62b52d52e57
h-n-tiwari <pmgc590@gmail.com>
2026-08-24 18:18:17 +0530

    Communicate with the BlockChain

---
+++ src/blockchain/indexer.ts
@@ -0,0 +1,29 @@
+import { ethers } from "ethers";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const provider = new ethers.JsonRpcProvider(
+  process.env.BLOCKCHAIN_RPC_URL
+);
+
+export async function getLatestBlock() {
+  return await provider.getBlockNumber();
+}
+
+export async function getBlock(blockNumber: number) {
+  const block = await provider.getBlock(
+    blockNumber,
+    true
+  );
+
+  if (!block) {
+    throw new Error(
+      `Block ${blockNumber} not found`
+    );
+  }
+
+  return {
+
+  }
+}
